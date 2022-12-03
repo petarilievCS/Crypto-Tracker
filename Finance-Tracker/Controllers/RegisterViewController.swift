35311e7d84b9b9f30325cec6c1e0f8861add1bb5
Petar Iliev <petariliev2002@gmail.com>
2022-12-03 14:18:13 -0600

    Setup Firebase, implement name check

--- Finance-Tracker/Controllers/RegisterViewController.swift
+++ Finance-Tracker/Controllers/RegisterViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import AudioToolbox
 
 class RegisterViewController: UIViewController {
     
@@ -23,9 +27,6 @@ class RegisterViewController: UIViewController {
         // Dismiss keyboard upon tap
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
-
-        // Hide back button
-        navigationItem.hidesBackButton = true
         
         // Customize buttons and text fields
         firstNameField.layer.cornerRadius = K.cornerRadius
@@ -36,9 +37,52 @@ class RegisterViewController: UIViewController {
         signUpButton.layer.cornerRadius = K.cornerRadius
     }
     
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        
+        // Check for validity of information
+        if checkName() {
+            performSegue(withIdentifier: "registerToStocks", sender: self)
+        } else {
+            // vibrate
+            AudioServicesPlaySystemSound(1519)
+        }
+        
+    }
+    
     // Dismiss keyboard upon tap
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    // Checks if the user entered a valid name and last name
+    func checkName() -> Bool {
+        var nameIsValid = true
+        
+        if firstNameField.text?.isEmpty ?? true {
+            firstNameField.placeholder = "Please enter your name..."
+            shake(firstNameField)
+            nameIsValid = false
+        }
+        
+        if lastNameField.text?.isEmpty ?? true {
+            lastNameField.placeholder = "Please enter your last name..."
+            shake(lastNameField)
+            nameIsValid = false
+        }
+        
+        return nameIsValid
+        
+    }
+    
+    // Creates a basic shake animation for the text fields
+    func shake(_ viewToShake: UITextField) {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 4
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: viewToShake.center.x - 10, y: viewToShake.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: viewToShake.center.x + 10, y: viewToShake.center.y))
+        viewToShake.layer.add(animation, forKey: "position")
     }
 
 }
